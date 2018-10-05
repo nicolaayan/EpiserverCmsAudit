566264027fb487d1548729b157e6fc254709b166
Allan Thraen <allan@codeart.dk>
2018-10-05 13:19:18 +0200

    Updated with build script and some final changes

---
+++ nuget/ZipBuildAndPack.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+cls
+
+echo Creating the module zip file..
+powershell -command "Compress-Archive -Path ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\Views, ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\module.config -DestinationPath ..\N1990.Episerver.Cms.Audit\Modules\_protected\CmsAudit\CmsAudit -CompressionLevel Optimal -Force"
+echo.
+
+echo Creating NuGet package..
+echo.
+nuget pack N1990.Episerver.Cms.Audit.nuspec 
