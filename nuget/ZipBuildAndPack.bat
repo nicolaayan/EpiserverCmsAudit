0576184199587c602023187d7729501a9793b660
Allan Thraen <allan@codeart.dk>
2018-10-22 17:58:44 +0200

    Worked around issue with the error: CS0103. It was related to having the moduled zipped. Might be similar to this one: https://world.episerver.com/forum/developer-forum/Developer-to-developer/Thread-Container/2016/5/addon-with-zipped-content-folder-and-razor-views/ although not fixed in newer version....

--- nuget/ZipBuildAndPack.bat
+++ nuget/ZipBuildAndPack.bat
@@ -2,7 +2,7 @@
 cls
 
 echo Creating the module zip file..
-powershell -command "Compress-Archive -Path ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\Views, ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\module.config -DestinationPath ..\N1990.Episerver.Cms.Audit\Modules\_protected\CmsAudit\CmsAudit -CompressionLevel Optimal -Force"
+# powershell -command "Compress-Archive -Path ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\Views, ..\N1990.Episerver.Cms.Audit\modules\_protected\CmsAudit\module.config -DestinationPath ..\N1990.Episerver.Cms.Audit\Modules\_protected\CmsAudit\CmsAudit -CompressionLevel Optimal -Force"
 echo.
 
 echo Creating NuGet package..
